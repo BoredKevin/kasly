0fa8b7fc5144ba3acd15cdca2d9439dedb2cd547
BoredKevin <kevin@boredkevin.com>
2026-08-23 22:48:48 +0700

    refactor: remove starter template numbers and resource components

--- src/features/numbers/index.ts
+++
@@ -1,1 +0,0 @@
-export { NumberGenerator } from "./components/NumberGenerator";
--- src/features/resources/index.ts
+++
@@ -1,3 +0,0 @@
-export { ResourceCard } from "./components/ResourceCard";
-export { ResourcesGrid } from "./components/ResourcesGrid";
-export { columnOneResources, columnTwoResources } from "./data/resourcesData";
